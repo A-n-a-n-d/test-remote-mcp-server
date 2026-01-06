0b12d390209a9a3466d8d03d0dfa6d613c7142d0
Ru326 <dhander326@gmail.com>
2026-01-06 20:59:26 +0530

    added comment for the tool

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ from fastmcp import FastMCP
 # Create a FastMCP server instance
 mcp = FastMCP(name="Remote Server - Demo2")
 
+# create tool
 @mcp.tool # <--- (Red arrow points here)
 def roll_dice(n_dice: int = 1) -> list[int]:
     """Roll n_dice 6-sided dice and return the results."""
